7864cb65b377b047fa51d2d947f3015eb7fe1e2a
Natalia Tarasova <natalia.tarasova@mechanica.ai>
2022-05-12 18:31:11 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='spam_detector',
+    version='',
+    packages=['spam_detector'],
+    url='',
+    license='',
+    author='natoshir',
+    author_email='',
+    description=''
+)
