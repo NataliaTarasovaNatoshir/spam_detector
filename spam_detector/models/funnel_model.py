46343d70d453ea2c0cc3913b44974a8415a526e5
Natalia Tarasova <natalia.tarasova@mechanica.ai>
2022-05-22 17:13:13 +0300

    add funnel approach

---
+++ spam_detector/models/funnel_model.py
@@ -0,0 +1,211 @@
+# remember to install dependencies
+"""
+# A dependency of the preprocessing for BERT inputs
+!pip install -q -U "tensorflow-text==2.8.*"
+
+# AdamW optimizer
+!pip install -q tf-models-official==2.7.0
+"""
+import os
+import tensorflow_text as text
+import tensorflow as tf
+import tensorflow_hub as hub
+from official.nlp import optimization  # to create AdamW optimizer
+
+tf.get_logger().setLevel('ERROR')
+
+map_name_to_handle = {
+    'bert_en_uncased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/3',
+    'bert_en_cased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_cased_L-12_H-768_A-12/3',
+    'bert_multi_cased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_multi_cased_L-12_H-768_A-12/3',
+    'small_bert/bert_en_uncased_L-2_H-128_A-2':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-2_H-128_A-2/1',
+    'small_bert/bert_en_uncased_L-2_H-256_A-4':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-2_H-256_A-4/1',
+    'small_bert/bert_en_uncased_L-2_H-512_A-8':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-2_H-512_A-8/1',
+    'small_bert/bert_en_uncased_L-2_H-768_A-12':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-2_H-768_A-12/1',
+    'small_bert/bert_en_uncased_L-4_H-128_A-2':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-128_A-2/1',
+    'small_bert/bert_en_uncased_L-4_H-256_A-4':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-256_A-4/1',
+    'small_bert/bert_en_uncased_L-4_H-512_A-8':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1',
+    'small_bert/bert_en_uncased_L-4_H-768_A-12':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-768_A-12/1',
+    'small_bert/bert_en_uncased_L-6_H-128_A-2':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-6_H-128_A-2/1',
+    'small_bert/bert_en_uncased_L-6_H-256_A-4':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-6_H-256_A-4/1',
+    'small_bert/bert_en_uncased_L-6_H-512_A-8':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-6_H-512_A-8/1',
+    'small_bert/bert_en_uncased_L-6_H-768_A-12':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-6_H-768_A-12/1',
+    'small_bert/bert_en_uncased_L-8_H-128_A-2':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-8_H-128_A-2/1',
+    'small_bert/bert_en_uncased_L-8_H-256_A-4':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-8_H-256_A-4/1',
+    'small_bert/bert_en_uncased_L-8_H-512_A-8':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-8_H-512_A-8/1',
+    'small_bert/bert_en_uncased_L-8_H-768_A-12':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-8_H-768_A-12/1',
+    'small_bert/bert_en_uncased_L-10_H-128_A-2':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-10_H-128_A-2/1',
+    'small_bert/bert_en_uncased_L-10_H-256_A-4':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-10_H-256_A-4/1',
+    'small_bert/bert_en_uncased_L-10_H-512_A-8':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-10_H-512_A-8/1',
+    'small_bert/bert_en_uncased_L-10_H-768_A-12':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-10_H-768_A-12/1',
+    'small_bert/bert_en_uncased_L-12_H-128_A-2':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-12_H-128_A-2/1',
+    'small_bert/bert_en_uncased_L-12_H-256_A-4':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-12_H-256_A-4/1',
+    'small_bert/bert_en_uncased_L-12_H-512_A-8':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-12_H-512_A-8/1',
+    'small_bert/bert_en_uncased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-12_H-768_A-12/1',
+    'albert_en_base':
+        'https://tfhub.dev/tensorflow/albert_en_base/2',
+    'electra_small':
+        'https://tfhub.dev/google/electra_small/2',
+    'electra_base':
+        'https://tfhub.dev/google/electra_base/2',
+    'experts_pubmed':
+        'https://tfhub.dev/google/experts/bert/pubmed/2',
+    'experts_wiki_books':
+        'https://tfhub.dev/google/experts/bert/wiki_books/2',
+    'talking-heads_base':
+        'https://tfhub.dev/tensorflow/talkheads_ggelu_bert_en_base/1',
+}
+map_model_to_preprocess = {
+    'bert_en_uncased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'bert_en_cased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_cased_preprocess/3',
+    'small_bert/bert_en_uncased_L-2_H-128_A-2':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-2_H-256_A-4':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-2_H-512_A-8':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-2_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-4_H-128_A-2':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-4_H-256_A-4':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-4_H-512_A-8':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-4_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-6_H-128_A-2':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-6_H-256_A-4':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-6_H-512_A-8':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-6_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-8_H-128_A-2':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-8_H-256_A-4':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-8_H-512_A-8':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-8_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-10_H-128_A-2':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-10_H-256_A-4':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-10_H-512_A-8':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-10_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-12_H-128_A-2':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-12_H-256_A-4':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-12_H-512_A-8':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'small_bert/bert_en_uncased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'bert_multi_cased_L-12_H-768_A-12':
+        'https://tfhub.dev/tensorflow/bert_multi_cased_preprocess/3',
+    'albert_en_base':
+        'https://tfhub.dev/tensorflow/albert_en_preprocess/3',
+    'electra_small':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'electra_base':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'experts_pubmed':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'experts_wiki_books':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+    'talking-heads_base':
+        'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3',
+}
+
+import bow_model, bert_model
+
+class BERTModel():
+    def __init__(self, config, model_name='', load_from_folder=False, folder=None):
+        self.bow_threshold = config["funnel_approach"]["bow_threshold"]
+        self.bert_threshold = config["funnel_approach"]["bert_threshold"]
+        self.bert_app_score_min = config["funnel_approach"]["bert_app_score_min"]
+        self.bert_app_score_max = config["funnel_approach"]["bert_app_score_max"]
+        self.bow_model_name = config['funnel_approach']['bow_model_name']
+        self.bert_model_name = config['funnel_approach']['bert_model_name']
+        if load_from_folder:
+            print("Loading the model from {}".format(folder))
+            self.model_name = folder.split('/')[-1]
+            print('Loading model for Bag of Words approach')
+            self.bow_model = bow_model.BoWModel(load_from_file=True,
+                                                filepath=os.path.join(folder, self.bow_model_name + '.joblib'))
+            print('Loading the model for Transformers approach')
+            self.bert_model = bert_model.BERTModel(load_from_folder=True,
+                                                   folder=os.path.join(folder, self.bert_model_name))
+            print('Model loaded successfully')
+        else:
+            self.model_name = model_name
+            print('Create Bag of Words approach model')
+            self.bow_model = bow_model.BoWModel(load_from_file=False,
+                                                model_name=self.bow_model_name,
+                                                vectorizer_params=dict(sublinear_tf=True, max_df=0.5,
+                                                                       stop_words="english", analyzer='word',
+                                                                       ngram_range=(1, 2)),
+                                                classifier_params=dict(max_iter=50, loss='modified_huber', alpha=0.0871))
+            print('Create Transformers approach model')
+            self.bert_model = bert_model.BERTModel(model_name=self.bert_model_name,
+                                                   bert_model_name='small_bert/bert_en_uncased_L-4_H-512_A-8')
+            self.bert_epochs = config['funnel_approach']['bert_epochs_num']
+            print('Model initiated successfully')
+
+    def train(self, X, y):
+        print('Training Bag of Words model')
+        self.bow_model.train(X, y)
+        print('Train BERT model')
+        self.bert_model.train(X, y, epochs=self.bert_epochs)
+
+    def predict(self, X):
+        # make predictions with bow
+        y_pred_bow = self.bow_model.predict(X)
+        y_pred_label = y_pred_bow >= self.bow_threshold
+        use_bert_index = (y_pred_bow > self.bert_app_score_min) & (y_pred_bow <= self.bert_app_score_max)
+        y_pred_bert = self.bert_model.predict(X.iloc[use_bert_index, :])
+        y_pred_label[use_bert_index] = y_pred_bert.numpy().flatten()
+        return y_pred_label
+
+    def dump(self, folder):
+        model_folder = os.path.join(folder, self.model_name)
+        print('Save Bag of Words model')
+        self.bow_model.dump(model_folder)
+        print('Save BERT model')
+        self.bert_model.dump(model_folder)
+
+        print('Model saved to {}'.format(model_folder))
--- spam_detector/quality_assessment.py
+++ spam_detector/quality_assessment.py
@@ -1,10 +1,10 @@
 import os
 import pandas as pd
 from datetime import datetime
-from sklearn.metrics import roc_auc_score, precision_recall_curve, confusion_matrix
+from sklearn.metrics import roc_auc_score, precision_recall_curve, confusion_matrix, recall_score, precision_score
 
 
-def run_experiment(config, model_name, model, return_predictions=False):
+def run_experiment(config, model_name, model, return_predictions=False, use_labels=False):
     print("Testing model {}\n".format(model_name))
     print("Loading test dataset")
     test = pd.read_csv(os.path.join(config['dataset_build']['res_dataset_folder_name'], 'test.csv'))
@@ -25,19 +25,27 @@ def run_experiment(config, model_name, model, return_predictions=False):
     # binary classification metrics
     y_true = test['label'].values
     print('Binary classification metrics:')
-    # roc-auc as a general quality metric
-    roc_auc = roc_auc_score(y_true, y_pred)
-    print("roc-auc score: {0:.4f}".format(roc_auc))
-    # calculate precision at a chosen recall level
-    precision, recall, thresholds = precision_recall_curve(y_true, y_pred)
-    i = 0
-    for i in range(len(recall) - 1):
-        if recall[i] <= config['testing']['recall_level']: break
-    selected_threshold = thresholds[i]
-    selected_recall = recall[i]
-    selected_precision = precision[i]
-    print('At recall {0:.4f} precision = {1:.4f}\n'.format(selected_recall, selected_precision))
-    print('Confusion matrix:\n{}'.format(confusion_matrix(y_true, y_pred >= selected_threshold)))
+    if not use_labels:
+        # roc-auc as a general quality metric
+        roc_auc = roc_auc_score(y_true, y_pred)
+        print("roc-auc score: {0:.4f}".format(roc_auc))
+        # calculate precision at a chosen recall level
+        precision, recall, thresholds = precision_recall_curve(y_true, y_pred)
+        i = 0
+        for i in range(len(recall) - 1):
+            if recall[i] <= config['testing']['recall_level']: break
+        selected_threshold = thresholds[i]
+        selected_recall = recall[i]
+        selected_precision = precision[i]
+        print('At recall {0:.4f} precision = {1:.4f}\n'.format(selected_recall, selected_precision))
+        print('Confusion matrix:\n{}'.format(confusion_matrix(y_true, y_pred >= selected_threshold)))
+    else:
+        roc_auc = None
+        selected_threshold = None
+        selected_recall = recall_score(y_true, y_pred)
+        selected_precision = precision_score(y_true, y_pred)
+        print('At recall {0:.4f} precision = {1:.4f}\n'.format(selected_recall, selected_precision))
+        print('Confusion matrix:\n{}'.format(confusion_matrix(y_true, y_pred)))
 
     results = {'model_name': model_name, 'test_dataset': {'size': len(test), 'spam_share': test['label'].mean()},
             'runtime': {'total_runtime': runtime, 'runtime_per_1000': runtime_per_1000},
